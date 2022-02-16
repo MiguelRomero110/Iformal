74f47e379c58d87b02e84e4c90e958d54fbbed84
Miguel <mromeroc@esliceu.net>
2022-02-16 09:28:50 +0100

    footer

--- footer.php
+++ footer.php
@@ -8,14 +8,14 @@
             <div class="col-md-3 footer3">
             <?php
     wp_nav_menu( array(
-      'theme_location'  => 'infobasic_footer_interno',
+      'theme_location'  => 'ifromal_footer_interno',
   ) );
     ?>
             </div>
             <div class="col-md-3 footer3">
             <?php
     wp_nav_menu( array(
-      'theme_location'  => 'infobasic_footer_externo',
+      'theme_location'  => 'iformal_footer_externo',
    ) );
     ?>
             </div>
