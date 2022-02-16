338e31f571b297bd997037f15da2438712219097
Miguel <mromeroc@esliceu.net>
2022-02-16 08:58:23 +0100

    1.0
    primer paso

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 footer6">
+                <p>Ir al inicio</p>
+                <a href="home">Tienda</a>
+            </div>
+            <div class="col-md-3 footer3">
+            <?php
+    wp_nav_menu( array(
+      'theme_location'  => 'infobasic_footer_interno',
+  ) );
+    ?>
+            </div>
+            <div class="col-md-3 footer3">
+            <?php
+    wp_nav_menu( array(
+      'theme_location'  => 'infobasic_footer_externo',
+   ) );
+    ?>
+            </div>
+        </div>
+    </div>
+    <div>
+    </div>
+</footer>
+<script src="https://kit.fontawesome.com/f40d12f917.js" crossorigin="anonymous"></script>
+<?php wp_footer();?>
+</body>
+
+</head>
